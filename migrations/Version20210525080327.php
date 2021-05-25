0832a865309a35f9727d854bce98b3e9700b05fc
Victor <stutz.vic@gmal.com>
2021-05-25 14:27:04 +0200

    Contact

--- migrations/Version20210525080327.php
+++ migrations/Version20210525080327.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210521135321 extends AbstractMigration
+final class Version20210525080327 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20210521135321 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE article_accueil (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, texte LONGTEXT NOT NULL, image VARCHAR(255) DEFAULT NULL, position VARCHAR(255) DEFAULT NULL, active TINYINT(1) NOT NULL, bouton_texte VARCHAR(255) DEFAULT NULL, bouton_lien VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE article_accueil (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, texte LONGTEXT NOT NULL, image VARCHAR(255) DEFAULT NULL, updated_at DATETIME NOT NULL, position VARCHAR(255) DEFAULT NULL, active TINYINT(1) NOT NULL, bouton_texte VARCHAR(255) DEFAULT NULL, bouton_lien VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
